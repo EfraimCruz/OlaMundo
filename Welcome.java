55358ca6396e0346cc97b5b7f1278c3c8b50ea74
efraimcruz <efraimcruz05@gmail.com>
2024-06-26 20:21:34 -0300

    implementação do hello World utilizando metodo

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Welcome{
     public static void main(String[]args){
+
+        Scanner entrada = new Scanner(System.in);
+        String nome;
+
         System.out.println("\nHello, Wolrd");
+
+        System.out.println("Digite seu nome ");
+        nome = entrada.nextLine();
+
+        System.out.println("Hello Wolrd e Hello "+nome);
+
 
     }
 }
